2f24c389a7f00d5e4474db4496b76f82bf86c080
U-BSD\455138 <455138@SHS-BYBF6X3.beaverton.k12.or.us>
2026-01-08 10:20:54 -0800

    Initial commit for challenge3.py

---
+++ challenge3.py
@@ -0,0 +1,12 @@
+"""
+Description: Find the key for a single-byte XOR cipher
+Author: Aahana Sapra
+Date: 1/8/2026
+"""
+
+# initialize hex string
+hexStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+# def bytes to ASCII conversion function
+def bytesToASCII(bytesArr):
+    return bytes.decode('ascii')
