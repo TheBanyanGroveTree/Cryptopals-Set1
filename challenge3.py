267264406e51650bd36d30b576176d2c79305569
U-BSD\455138 <455138@SHS-94W2NW3.beaverton.k12.or.us>
2026-01-13 10:22:45 -0800

    Add hexToBytes() function and initialize ASCII arr

--- challenge3.py
+++ challenge3.py
@@ -7,6 +7,15 @@ Date: 1/8/2026
 # initialize hex string
 hexStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
+# initialize bytes object containing every char in ASCII table
+asciiTable = bytes(range(128))
+
+# define hex to bytes conversion function
+def hexToBytes(hexStr):
+    return bytes.fromhex(hexStr)
+
 # def bytes to ASCII conversion function
 def bytesToASCII(bytesArr):
-    return bytes.decode('ascii')
+    return bytesArr.decode('ascii')
+
+
