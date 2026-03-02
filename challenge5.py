7a0e61767a14ae0271ff930f19b97c8ecc86c0da
U-BSD\455138 <455138@SHS-BYBF6X3.beaverton.k12.or.us>
2026-03-02 09:33:35 -0800

    Add data type print statements

--- challenge5.py
+++ challenge5.py
@@ -18,13 +18,17 @@ def repeatedKeyXOR(plaintext, key):
     encoded = []
 
     for i in range(len(plaintext)):
+        print("plaintext data type:", type(plaintext[i]))
+        print("key data type:", type(key[i % keyLen]))
         encoded.append(plaintext[i] ^ key[i % keyLen])
 
     return bytes(encoded)
 
 # convert strings to bytes
 plaintext = asciiToBytes("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
+print(type(plaintext))
 key = asciiToBytes("ICE")
+print(type(key))
 
 # output results
 print(bytesToHex(repeatedKeyXOR(plaintext, key)))
